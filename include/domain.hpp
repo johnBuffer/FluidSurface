fa3b504948949eb0abb2b582a4c96725001e2e9e
Jean Tampon <jean.tampon@gmail.com>
2020-10-20 01:03:38 +0200

    graphics update

--- include/domain.hpp
+++ include/domain.hpp
@@ -1,18 +1,53 @@
 #pragma once
 #include <vector>
 
+
 struct Column
 {
 	float height;
 	float velocity;
 	float next_height;
+	float moved;
 
 	Column()
 		: height(0.0f)
 		, velocity(0.0f)
 		, next_height(0.0f)
+		, moved(0.0f)
 	{
 
+	}
+
+	float getHeight() const
+	{
+		return height + moved;
+	}
+};
+
+struct Object
+{
+	float x, y;
+	float mass;
+	float vx, vy;
+	float radius;
+
+	Object()
+		: x(0.0f)
+		, y(0.0f)
+		, mass(1.0f)
+		, vx(0.0f)
+		, vy(0.0f)
+		, radius(0.0f)
+	{}
+
+	void update(float dt)
+	{
+		const float gravity = 10.0f;
+		vx += gravity * dt;
+		vy += gravity * dt;
+
+		x += vx * dt;
+		y += vy * dt;
 	}
 };
 
@@ -21,6 +56,7 @@ struct Domain
 	float width;
 	float vel;
 	std::vector<Column> columns;
+	std::vector<Object> objects;
 
 	Domain(uint32_t count, float column_width, float velocity)
 		: width(column_width)
@@ -34,20 +70,52 @@ struct Domain
 
 	float getHeight(uint32_t i) const
 	{
-		return columns[i].height;
+		return columns[i].getHeight();
+	}
+
+	void checkObjectsCollisions()
+	{
+		int32_t i = 0;
+		float x = width * 0.5f;
+		for (Column& c : columns) {
+			for (Object& o : objects) {
+				const float dx = std::abs(x - o.x);
+				// If the object overlaps the column
+				if (dx < o.radius) {
+					// Compute the immerged height
+					const float height = sqrt(o.radius * o.radius - dx * dx);
+					const float bottom = o.y + height;
+					const float dy = bottom - (900 - c.height);
+					const float moved = std::min(std::max(dy, 0.0f), 2.0f * height);
+					if (i > 0 && i < columns.size() - 1) {
+						columns[i + 1].height += moved * 0.5f;
+						columns[i - 1].height += moved * 0.5f;
+					}
+					columns[i].height -= moved;
+				}
+				else {
+					c.moved = 0.0f;
+				}
+			}
+
+			++i;
+			x += width;
+		}
 	}
 
 	void update(float dt)
 	{
+		checkObjectsCollisions();
+
 		const int64_t count = columns.size() - 1;
 		const float v2 = vel * vel;
 		const float inv_width = 1.0f / (width * width);
 
-		const float k = -0.90f;
+		const float k = 0.50f;
 		
 		for (int64_t i(1); i < count; ++i) {
 			float f = v2 * (getHeight(i - 1) + getHeight(i + 1) - 2.0f * getHeight(i)) * inv_width;
-			f += k * columns[i].velocity;
+			f -= k * columns[i].velocity;
 			columns[i].velocity = columns[i].velocity + f * dt;
 		}
 
@@ -55,7 +123,7 @@ struct Domain
 		{
 			// Should be OK if more than 1 columns
 			float f = v2 * (columns[0].height + columns[1].height - 2.0f * columns[0].height) * inv_width;
-			f += k * columns[0].velocity;
+			f -= k * columns[0].velocity;
 			columns[0].velocity = columns[0].velocity + f * dt;
 		}
 
@@ -63,12 +131,12 @@ struct Domain
 		{
 			// Should be OK if more than 1 columns
 			float f = v2 * (columns[count-1].height + columns[count].height - 2.0f * columns[count].height) * inv_width;
-			f += k * columns[count].velocity;
+			f -= k * columns[count].velocity;
 			columns[count].velocity = columns[count].velocity + f * dt;
 		}
 
 		for (Column& c : columns) {
-			c.height += c.velocity;
+			c.height += c.velocity * dt;
 		}
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,24 @@ int main()
 {
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	constexpr uint32_t WinWidth = 1600;
-	constexpr uint32_t WinHeight = 900;
+	constexpr uint32_t WinWidth = 1920;
+	constexpr uint32_t WinHeight = 1080;
 
-	sf::RenderWindow window(sf::VideoMode(WinWidth, WinHeight), "FluidSurface", sf::Style::Default, settings);
+	sf::RenderWindow window(sf::VideoMode(WinWidth, WinHeight), "FluidSurface", sf::Style::Fullscreen, settings);
 	window.setVerticalSyncEnabled(true);
 
-	Domain domain(200, 8, 70.0f);
+	Domain domain(192, 11, 800.0f);
+
+	//domain.objects.emplace_back();
+	//domain.objects.back().radius = 150.0f;
+
+	const float normalizer = 1.0f / sqrt(2.0f);
+	const sf::Vector2f sun_direction(-1.0f * normalizer, 1.0f * normalizer);
 	
 	while (window.isOpen())
 	{
+		sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -24,17 +32,20 @@ int main()
 				window.close();
 			}
 			else if (event.type == sf::Event::MouseButtonPressed) {
-				const float amount = 200.0f;
-				sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+				float amount = 200.0f;
+				if (event.mouseButton.button == sf::Mouse::Right) {
+					amount *= -1.0f;
+				}
 				const uint64_t col = mouse_position.x / domain.width;
-				const int32_t width = 20;
+				const int32_t width = 30;
 				const uint32_t wave_width = 2 * width + 1;
 				const float PI = 3.14159f;
 				for (int32_t i(-width); i <= width; ++i) {
 					int32_t index = col + i;
 					if (index >= 0 && index < domain.columns.size()) {
 						const float ratio = i / float(wave_width);
-						domain.columns[index].height += 0.05f * amount * std::pow(cos(2.0f * ratio * PI), 8.0f) + 0.95f * amount * std::pow(cos(ratio * PI), 2.0f);
+						domain.columns[index].height += amount * std::pow(cos(ratio * PI), 3.0f);
+						domain.columns[index].height += 0.025f * amount * std::pow(cos(4.0f * ratio * PI), 3.0f);
 					}
 				}
 			}
@@ -55,15 +66,52 @@ int main()
 
 		window.clear();
 		
-		float x = 0.0f;
-		for (const Column& c : domain.columns) {
-			sf::RectangleShape r(sf::Vector2f(domain.width, c.height));
-			r.setFillColor(sf::Color::Blue);
-			r.setOrigin(0.0f, c.height);
-			r.setPosition(x, WinHeight);
-			window.draw(r);
+		const uint64_t count = domain.columns.size() - 1;
+		sf::VertexArray va(sf::Quads, 4 * count);
+		for (uint32_t i(0); i < count; ++i) {
+			uint32_t  index = 4 * i;
+			const Column& c = domain.columns[i];
+			const Column& next_c = domain.columns[i+1];
+			va[index + 0].position = sf::Vector2f(i * domain.width, WinHeight);
+			va[index + 1].position = sf::Vector2f(i * domain.width, WinHeight - c.height);
+			va[index + 2].position = sf::Vector2f((i + 1) * domain.width, WinHeight - next_c.height);
+			va[index + 3].position = sf::Vector2f((i + 1) * domain.width, WinHeight);
 
-			x += domain.width;
+			va[index + 0].color = sf::Color::Blue;
+			va[index + 1].color = sf::Color::Blue;
+			va[index + 2].color = sf::Color::Blue;
+			va[index + 3].color = sf::Color::Blue;
+		}
+		window.draw(va);
+
+		const float width = 20.0f;
+		sf::VertexArray va_spec(sf::TriangleStrip, 2 * (count + 1));
+		va_spec[0].position = sf::Vector2f(0.0f, WinHeight - domain.columns[0].height);
+		va_spec[0].color = sf::Color::Blue;
+		va_spec[1].position = sf::Vector2f(0.0f, WinHeight - domain.columns[0].height + width);
+		va_spec[1].color = sf::Color::Blue;
+		for (uint32_t i(1); i < count+1; ++i) {
+			uint32_t index = 2*i;
+			const Column& c = domain.columns[i];
+
+			va_spec[index + 0].position = sf::Vector2f(i * domain.width, WinHeight - c.height);
+			const sf::Vector2f v = va_spec[index].position - va_spec[index - 2].position;
+			const float length = sqrt(v.x*v.x + v.y*v.y);
+			const sf::Vector2f normal(-v.y / length, v.x / length);
+			const float dot = std::pow(std::max(0.0f, normal.x * sun_direction.x + normal.y * sun_direction.y), 4.0f);
+			va_spec[index + 1].position = va_spec[index + 0].position + normal * width * dot;
+
+			va_spec[index + 0].color = sf::Color(dot * 255, dot * 255, 255);
+			va_spec[index + 1].color = sf::Color::Blue;
+		}
+		window.draw(va_spec);
+
+		for (const Object& o : domain.objects) {
+			sf::CircleShape circle(o.radius);
+			circle.setOrigin(o.radius, o.radius);
+			circle.setPosition(o.x, o.y);
+			circle.setFillColor(sf::Color(255, 255, 0, 128));
+			window.draw(circle);
 		}
 
 		window.display();
